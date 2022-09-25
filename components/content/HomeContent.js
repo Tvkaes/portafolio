ca7aca2fdcb3b02cedc30cbcf012d2d09699bbfa
tvkaes <dertvk8s@gmail.com>
2022-09-24 19:16:12 -0700

    Font Bug again

--- components/content/HomeContent.js
+++ components/content/HomeContent.js
@@ -8,8 +8,8 @@ const HomeContent =(props)=>{
     return(
         <div>
           <div className={styles.containerTitle}>
-            <Typography variant="h3" className={styles.title}>I want to be a fullstack developer</Typography>
-            <Typography variant="subtitle2" className={styles.upSubtitle} >Hey, I&#39;M Jonathan</Typography>
+            <Typography variant="h3" className={styles.title} style={{fontFamily:'MontserratBold'}}>I want to be a fullstack developer</Typography>
+            <Typography variant="subtitle2" className={styles.upSubtitle} style={{fontFamily:'MontserratBold'}} >Hey, I&#39;M Jonathan</Typography>
           </div>
         </div>
     )
