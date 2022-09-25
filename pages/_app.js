f88a37ae79cf8005344b5bd427c8208a78c836a1
tvkaes <dertvk8s@gmail.com>
2022-09-24 18:26:34 -0700

    Responsive page ,changing design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+/* eslint-disable react/jsx-no-undef */
+import React,{Component} from 'react'
 import '../styles/globals.css'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function MyApp(props) {
+
+  const {Component,pageProps} = props
+
+  const wrappedComponent = Component.Layout ? (
+    <Component.Layout>
+      <Component {...pageProps} />
+    </Component.Layout>
+  ) : (
+    <Component {...pageProps} />
+  )
+
+  return(
+    <React.Fragment>
+      <Head>
+      <title>Tvkaes Portafolio</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      {wrappedComponent}
+    </React.Fragment>
+  )
+  
 }
 
-export default MyApp
+
